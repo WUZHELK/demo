2cea63024e3d9bd36c8ef35907d5cf0c77e0671d
wuzhe <443429397@qq.com>
2023-09-14 20:22:38 +0800

    新增常量池类；增加文件写入实例；引入validation

--- src/main/java/com/example/demo/example/WriteFilesExample.java
+++ src/main/java/com/example/demo/example/WriteFilesExample.java
@@ -3,12 +3,14 @@ package com.example.demo.example;
 import cn.hutool.core.collection.CollectionUtil;
 import cn.hutool.core.date.DateUtil;
 import com.example.demo.util.FileUtils;
+import com.google.common.collect.Maps;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -16,44 +18,74 @@ import java.util.concurrent.Executors;
 @Slf4j
 public class WriteFilesExample {
 
-    public static void main(String[] args) {
-        log.info("start");
-        System.currentTimeMillis();
-        ExecutorService executorService = Executors.newSingleThreadExecutor();
-        executorService.execute(() -> {
-            try {
-//                test("D:\\Test\\index.txt", "D:\\Test\\data.txt", "每日卡片激活文件");
-                test("D:\\Test\\index2.txt", "D:\\Test\\data2.txt", "自动续卡审核检查失败卡片列表（审核阶段）");
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
-
-        executorService.shutdown();
+    // 每日卡片激活文件
+    private static final Map<String, String> map1 = Maps.newHashMap();
+    static {
+        map1.put("index", "D:\\Test\\index.txt");
+        map1.put("data", "D:\\Test\\data.txt");
+        map1.put("flag", "每日卡片激活文件");
     }
 
-    private static void test(String fileInfoPath, String dataInfoPath, String flag) throws InterruptedException {
+    // 自动续卡审核检查失败卡片列表（审核阶段）
+    private static final Map<String, String> map2 = Maps.newHashMap();
+    static {
+        map2.put("index", "D:\\Test\\index2.txt");
+        map2.put("data", "D:\\Test\\data2.txt");
+        map2.put("flag", "自动续卡审核检查失败卡片列表（审核阶段）");
+    }
+
+    // 自动续卡审核检查失败卡片列表(制卡阶段)
+    private static final Map<String, String> map3 = Maps.newHashMap();
+    static {
+        map3.put("index", "D:\\Test\\index3.txt");
+        map3.put("data", "D:\\Test\\data3.txt");
+        map3.put("flag", "自动续卡审核检查失败卡片列表(制卡阶段)");
+    }
+
+    // 自动续卡检查通过卡片列表（审核阶段）
+    private static final Map<String, String> map4 = Maps.newHashMap();
+    static {
+        map4.put("index", "D:\\Test\\index4.txt");
+        map4.put("data", "D:\\Test\\data4.txt");
+        map4.put("flag", "自动续卡检查通过卡片列表（审核阶段）");
+    }
+
+    // 自动续卡检查通过卡片列表（制卡阶段）
+    private static final Map<String, String> map5 = Maps.newHashMap();
+    static {
+        map5.put("index", "D:\\Test\\index5.txt");
+        map5.put("data", "D:\\Test\\data5.txt");
+        map5.put("flag", "自动续卡检查通过卡片列表（制卡阶段）");
+    }
+
+    private static void test(Map<String, String> map) throws InterruptedException {
+        String indexInfoPath = map.get("index");
+        String dataInfoPath = map.get("data");
+        String flag = map.get("flag");
+
         // 文件基本信息
-        List<String> indexList = FileUtils.readFile(fileInfoPath);
+        List<String> indexList = FileUtils.readFile(indexInfoPath);
         // 文件数据基本信息
         List<String> dataList = FileUtils.readFile(dataInfoPath);
 
         if (CollectionUtil.isNotEmpty(dataList)) {
             if ("每日卡片激活文件".equals(flag)) {
                 test1(dataList, indexList, 0);
-            } else if ("自动续卡审核检查失败卡片列表（审核阶段）".equals(flag)) {
+            }
+            else if ("自动续卡审核检查失败卡片列表（审核阶段）".equals(flag)) {
                 test1(dataList, indexList, 0);
-            } else if ("自动续卡审核检查失败卡片列表(制卡阶段)".equals(flag)) {
+            }
+            else if ("自动续卡审核检查失败卡片列表(制卡阶段)".equals(flag)) {
 
-            } else if ("自动续卡检查通过卡片列表（审核阶段）".equals(flag)) {
+            }
+            else if ("自动续卡检查通过卡片列表（审核阶段）".equals(flag)) {
 
-            } else if ("自动续卡检查通过卡片列表（制卡阶段）".equals(flag)) {
+            }
+            else if ("自动续卡检查通过卡片列表（制卡阶段）".equals(flag)) {
 
             }
 
         }
-
-
     }
 
     /**
@@ -97,6 +129,29 @@ public class WriteFilesExample {
         if(FileUtils.uploadFile(sb.toString(), fileName)){
             log.info("write success");
         }
+    }
+
+    public static void main(String[] args) {
+        log.info("start");
+        System.currentTimeMillis();
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.execute(() -> {
+            try {
+                /**
+                 * 每日卡片激活文件-map1
+                 * 自动续卡审核检查失败卡片列表（审核阶段）-map2
+                 * 自动续卡审核检查失败卡片列表(制卡阶段)-map3
+                 * 自动续卡检查通过卡片列表（审核阶段）-map4
+                 * 自动续卡检查通过卡片列表（制卡阶段）-map5
+                 *
+                 */
+                test(map1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        executorService.shutdown();
     }
 
 }
