5e1cdfe9ccefbf7bb86bd3a1bfa155c7658c66ed
wuzhe <443429397@qq.com>
2023-09-15 15:21:03 +0800

    文件生成工具类修改为统一入口

--- src/main/java/com/example/demo/example/WriteFilesExample.java
+++ src/main/java/com/example/demo/example/WriteFilesExample.java
@@ -4,7 +4,6 @@ import cn.hutool.core.date.DateUtil;
 import com.example.demo.bean.dto.FileDTO;
 import com.example.demo.constant.Constant;
 import com.example.demo.util.FileUtils;
-import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Maps;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
