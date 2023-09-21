da29c18242f1c418d084f747e100a66ba4fd11d4
wuzhe <443429397@qq.com>
2023-09-21 15:47:11 +0800

    增加地址解析方法

--- src/main/java/com/example/demo/service/impl/MeiMeiFileServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/MeiMeiFileServiceImpl.java
@@ -10,9 +10,12 @@ import com.example.demo.service.MeiMeiFileService;
 import com.example.demo.util.ExcelUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.compress.utils.Lists;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 @Service
@@ -49,7 +52,13 @@ public class MeiMeiFileServiceImpl implements MeiMeiFileService {
 
                             String[] addressArr = new String[]{};
                             if (ObjectUtil.isNotEmpty(a) && ObjectUtil.isNotEmpty(a.getCnAddress())) {
-                                addressArr = a.getCnAddress().split(" ");
+                                // 不包含空格的自动识别进行空格填充
+                                String cnAddress = a.getCnAddress();
+                                if (StringUtils.isNotBlank(cnAddress)) {
+                                    cnAddress = getAddressWithEmpty(cnAddress);
+                                }
+                                // 处理分割符
+                                addressArr = cnAddress.split(" ");
                             }
                             List<String> addressList = Arrays.asList(addressArr);
                             if (CollectionUtil.isNotEmpty(addressList)) {
@@ -63,6 +72,8 @@ public class MeiMeiFileServiceImpl implements MeiMeiFileService {
                                 addressEntity.setStreet(addressList.size() > 3 ? addressList.get(3) : "");
                                 //详细地址
                                 addressEntity.setAddress(addressList.size() > 4 ? addressList.get(4) : "");
+                                //备用
+                                addressEntity.setBackup(addressList.size() > 5 ? addressList.get(5) : "");
                             }
                             writeList.add(addressEntity);
                         });
@@ -78,6 +89,25 @@ public class MeiMeiFileServiceImpl implements MeiMeiFileService {
             msg = e.getMessage();
         }
         return msg;
+    }
+
+    private String getAddressWithEmpty(String address) {
+        try {
+            List<String> arrs = new ArrayList<>(Arrays.asList("省", "市", "区", "县", "镇", "街道"));
+            Iterator<String> iterator = arrs.iterator();
+            while (iterator.hasNext()) {
+                String arr = iterator.next();
+                Integer index = address.indexOf(arr);
+                if (index > 0 && !" ".equals(String.valueOf(address.charAt(index + arr.length())))) {
+                    StringBuilder stringBuilder = new StringBuilder(address);
+                    stringBuilder.insert((address.indexOf(arr) + 1), " ");
+                    address = stringBuilder.toString();
+                }
+            }
+        } catch (Exception e) {
+            log.error("getAddressWithEmpty is error: " + e.getMessage());
+        }
+        return address;
     }
 
 }
